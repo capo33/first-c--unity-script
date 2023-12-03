2e59b56b25925076c88e60ba931bc5816571578f
Mohamed <s2000588@edu.bc.fi>
2023-12-03 12:15:11 +0200

    first c# game script

---
+++ Assets/PrintToConsole.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintToConsole : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("I am the first Logger :=)");
+        Debug.Log("I am the second Logger :=)");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
